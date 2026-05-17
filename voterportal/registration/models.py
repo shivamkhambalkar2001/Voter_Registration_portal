d77660b6c7c8625742e4794a48dff293135ef02e
shivam <shivkhambalkar12@gmail.com>
2026-05-17 22:43:30 +0530

    updated project

--- voterportal/registration/models.py
+++ voterportal/registration/models.py
@@ -7,7 +7,6 @@ from datetime import date
 import random
 import string
 
-
 def generate_voter_id():
     voter_id = 'V'
     return voter_id + ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
